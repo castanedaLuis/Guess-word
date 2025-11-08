b80a5da53ea9be8f949cc423cec4230c47179525
Jose Luis Castañeda <programador1996@outlook.es>
2025-11-08 00:10:37 -0600

    Creating component and logic to create board

---
+++ src/App.jsx
@@ -0,0 +1,150 @@
+import './styles/style.css'
+import Footer from './components/Footer';
+import { useEffect, useState } from 'react';
+
+const BOARD_WIDTH = 5
+const DATA_RANDOM = ['Hello', 'House', 'SOTAY', 'Jikqa', 'Codin', 'LIQUA']
+
+function generatedWords(larger) {
+    const letters = 'abcdefghijklmnopqrstuvwxyzñ';
+    let words = [];
+    let result = ''
+    for (let y = 0; y < 10; y++) {
+        result = ''
+        for (let i = 0; i < larger; i++) {
+            const randomIndex = Math.floor(Math.random() * letters.length);
+            result += letters[randomIndex];
+        }
+        words.push(result)
+    }
+    return words
+}
+const solution = generatedWords(5)[Math.floor(Math.random() * 10)];
+
+export default function App() {
+    console.warn(solution);
+    const [widthBoard, setWithBoard] = useState(0)
+    const [keysWords, setKeysWords] = useState([])
+    const [gameOver, setGameOver] = useState(false)
+    const [heightBoard, setHeightBoard] = useState(Array(widthBoard).fill(null))
+    const [inputValue, setInputValue] = useState("");
+
+    const handleEventKeyPress = (event) => {
+        const key = event.key;
+        if (key === 'Backspace') {
+            setKeysWords((prev) => prev.slice(0, -1));
+            return;
+        }
+        if (key === 'Enter' && keysWords.length === 5) {
+            const newBoardHight = [...heightBoard];
+            newBoardHight[heightBoard.findIndex(value => value === null)] = keysWords;
+            //BOARD_HIGTH = newBoardHight
+            setHeightBoard([...newBoardHight])
+            setKeysWords([])
+        }
+
+        if (keysWords.length >= 5) return;
+
+        if (/^[a-zñ]$/i.test(key)) {
+            setKeysWords((prev) => [...prev, key]);
+        }
+        const isCorrect = solution === keysWords.join('')
+        if (isCorrect) {
+            setGameOver(true)
+        }
+    };
+
+    const handleRestart = () => {
+        setHeightBoard(Array(widthBoard).fill(null))
+        setKeysWords([])
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleEventKeyPress)
+        return () => window.removeEventListener('keydown', handleEventKeyPress)
+    }, [keysWords])
+
+    useEffect(() => {
+        setHeightBoard(Array(widthBoard).fill(null))
+    }, [widthBoard])
+
+    return (
+        <>
+            <main>
+                {
+                    widthBoard === 0
+                        ?
+                        <div className='choose-level'>
+                            <label>What level do you want to play?</label>
+                            <input type='number'
+                                min={1}
+                                max={10}
+                                onChange={(e) => setInputValue(e.target.value)}
+                            />
+                            <span style={{ fontSize: '12px' }}>The lower the number, the more difficult it will be.</span>
+                            <button className='btn-start' onClick={() => setWithBoard(parseInt(inputValue, 10))} > Start</button>
+                        </div>
+                        :
+                        <>
+                            <div>
+                                <h1 className='title'>Guess the Word 🎩</h1>
+                                <section className='game'>
+                                    {
+                                        heightBoard.map((chars, index) => {
+                                            const isCurrentRow = index === heightBoard.findIndex(val => val === null)
+                                            return <Line
+                                                key={index}
+                                                letters={isCurrentRow ? keysWords : chars ?? ''}
+                                                isFinal={!isCurrentRow && chars != null}
+                                                solution={solution}
+                                                index={index}
+                                                widthBoard={widthBoard}
+                                            />
+                                        })
+                                    }
+                                </section>
+                            </div>
+                            <div className='container-result'>
+                                <button onClick={handleRestart} className='btn-restart'>Reiniciar</button>
+                                <div className='container-result-solution'>
+                                    {
+
+                                        solution.split('').map((letter, index) => {
+                                            const exitsLetter = heightBoard.filter(item => item != null).flat().includes(letter)
+                                            const isVissible = keysWords.includes(letter) || exitsLetter
+                                            return (
+                                                <div key={index} className={`tile-solution`}>
+                                                    <span style={{ opacity: isVissible ? 1 : 0 }}> {letter}</span>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </>
+                }
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+function Line({ letters = [], isFinal, solution, widthBoard }) {
+    const tiles = []
+    for (let i = 0; i < BOARD_WIDTH; i++) {
+        const char = letters[i]
+        const className = char === solution[i] ?
+            'correct'
+            : solution.includes(char) ?
+                'close'
+                : char === null ? ''
+                    : char != undefined ?
+                        'incorrect' : ''
+
+        tiles.push(<div key={i} className={`tile ${className}`}>{char}</div>)
+    }
+    return (
+        <div className='line'>{tiles}</div>
+    )
+}
