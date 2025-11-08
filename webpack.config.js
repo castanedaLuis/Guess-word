32f9841c50d8cb883bdf0368dda44408714b587a
Jose Luis Castañeda <programador1996@outlook.es>
2025-11-08 00:09:39 -0600

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve("dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/i, // 👈 si usas CSS
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"], // 👈 permite importar sin escribir extensión
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+    devServer: {
+        static: "./dist",
+        open: true,
+    },
+};
