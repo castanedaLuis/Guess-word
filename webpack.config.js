4af80b443fe17742e3b442ea1c892c84097f0316
Jose Luis Castañeda <programador1996@outlook.es>
2025-11-10 11:39:29 -0600

    feat:

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ export default {
     resolve: {
         extensions: [".js", ".jsx"], // 👈 permite importar sin escribir extensión
     },
-    plugins: [new HtmlWebpackPlugin({ template: "./index.html" })],
+    plugins: [new HtmlWebpackPlugin({ template: "./index.html", filename: "index.html", })],
     devServer: {
         static: "./dist",
         open: true,
